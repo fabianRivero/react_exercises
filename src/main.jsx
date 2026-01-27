f33d9464951fb2645ec5dd43f257daf613e8bce9
fabian <fabianarj467@gmail.com>
2026-01-27 15:35:47 -0400

    ejercicios terminados

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter basename="/react_exercises">
+        <App />
+      </BrowserRouter>
   </StrictMode>,
 )
